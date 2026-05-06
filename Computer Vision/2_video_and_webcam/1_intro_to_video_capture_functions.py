a1b7c308ad75bcfa422e9d93597b89eca4ce61f4
waseem-development <hafizwaseemahmed2002@gmail.com>
2026-05-06 22:28:56 +0500

    Changed Folder Structure

--- Computer Vision/2_video_and_webcam/1_intro_to_video_capture_functions.py
+++ Computer Vision/2_video_and_webcam/1_intro_to_video_capture_functions.py
@@ -140,3 +140,22 @@
 # So:
 #   Image Processing → One Image
 #   Video Processing → Many Images (Frames in a loop)
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read() # ret means return: ret=True/False, frame=image
+
+    if not ret:
+        print("Could not read frame")
+        break
+    cv2.imshow("Webcam Freed", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        print("Quitting...")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ Computer Vision/2_video_and_webcam/2_processing_frame_by_frame.py
@@ -0,0 +1,10 @@
+# Frame by Frame Processing
+# Why?
+#  - Draw
+#  - Detect (faces, eyecolor)
+#  - Analyze
+#  - Specific Time
+
+# How to save a video as a file?
+
+import cv2
